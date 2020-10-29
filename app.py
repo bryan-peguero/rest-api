636525fde088f5426631aa35e6086fe8fce2c69b
Bryan Peguero <medranobryan22@gmail.com>
2020-10-29 15:48:08 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ from products import products
 def getProducts():
     return jsonify(products)
 
+#Find/Obtain a Product
 @app.route('/products/<string:product_name>')
 def getProduct(product_name):
     productFound = [product for product in products if product['name'] == product_name]
@@ -15,9 +16,32 @@ def getProduct(product_name):
         return jsonify({'product founded': productFound[0]})
     return jsonify({'message': 'Not Found'})
 
+#POST a product
 @app.route('/products', methods=['POST'])
 def addProduct():
-    return jsonify({'message':'it works'})
+    new_product = {"name": request.json['name'], 
+    "price": request.json['price'],
+    "description": request.json['description'],
+    "quantity": request.json['quantity'] 
+    }
+    products.append(new_product)
+    return jsonify({'message': 'Product added succesfully :D', 'products': products})
+
+#PUT a product
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if (len(productFound) > 0):
+        productFound[0]['name'] = request.json['name']
+        productFound[0]['price'] = request.json['price']
+        productFound[0]['description'] = request.json['description']
+        productFound[0]['quantity'] = request.json['quantity']
+        
+        return jsonify({
+            'message': 'Product updated',
+            'product updated': productFound[0]
+        })
+    return 'Not Found'
 
 #Init
 if __name__ == '__main__':
